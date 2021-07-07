ff409dbaf9cd1304bf340033e055930fa4198e83
Emmanuel VAÏSSE <evaisse@gmail.com>
2021-07-07 14:46:11 +0200

    [base-widget-tests] add test for travis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_app_test/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Go to the next 2 pages and get back home', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     await tester.pumpAndSettle();
